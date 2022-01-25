ec66058281549040a6805c5282af8c20f50931a0
tia54uba <93515097+tia54uba@users.noreply.github.com>
2022-01-25 05:02:56 +0000

    [update]5-1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
   config.hosts << "a8c9cb2b77b24968bf1f0967084f4a3f.vfs.cloud9.us-east-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+config.hosts << "390eb97d3fbd4296b223886496c3bc70.vfs.cloud9.us-east-1.amazonaws.com"
 end
---
+++ db/migrate/20220125042513_addtitle_tobooks.rb
@@ -0,0 +1,5 @@
+class AddtitleTobooks < ActiveRecord::Migration[6.1]
+  def change
+    add_column :books, :title, :string
+  end
+end
